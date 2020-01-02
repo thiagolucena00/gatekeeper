0fcc2da73fcfd584e3cb7f578b66b2c56582084b
Thiago Lucena <thiago.lucena_external@capgemini.com>
2020-01-02 18:13:10 +0000

    connection string for db connection

--- gatekeeper.business/app.js
+++ gatekeeper.business/app.js
@@ -1,7 +1,9 @@
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('config');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const businessRouter = require('./src/modules/business/business.route');
+const mongoose = require('mongoose');
 
 const pinoInspector = require('pino-inspector')
 const pino = require('express-pino-logger')({
@@ -10,8 +12,7 @@ const pino = require('express-pino-logger')({
     prettifier: pinoInspector
   });
 
-
-var bodyParser = require('body-parser');
+const businessRouter = require('./src/modules/business/business.route');
 
 var app = express();
 
@@ -25,5 +26,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/', businessRouter);
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.get('connectionString'), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection
+    .once('open', () => console.log('Mongodb connected!'))
+    .on('error', (error) => {
+        console.warn('Error : ', error);
+    });
 
 module.exports = app;
